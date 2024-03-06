f4869547d916ec15ca8e62c42e840336d274cfa9
Milena <milena@wapstore.com.br>
2024-03-06 07:55:16 -0300

    feat: add fonts and loading

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+/**
+ * Arquivo padrão do Expo Router para configurar o layout do app
+ */
+
+import { Slot } from 'expo-router'
+import { useFonts, Poppins_400Regular, Poppins_700Bold, Poppins_500Medium } from '@expo-google-fonts/poppins'
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_700Bold, Poppins_500Medium })
+  return fontsLoaded ? <Slot/> : null
+} 
